3f6dc5dc0fb8ff279c0d19367e982690d0022e8a
Tewzzz <114245077+Tewzzz@users.noreply.github.com>
2024-03-13 14:49:41 +0700

    Add files via upload

---
+++ lab1/src/App.js
@@ -0,0 +1,13 @@
+import './components/Form'
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
